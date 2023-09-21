ad127fec8e843899b28991ba9f336353c2baa27e
Christoffer Brevik <christoffer.brevik@tveco.net>
2023-09-21 12:08:35 +0200

    Added output to task 3.4

--- Task3/src/3.4.cpp
+++ Task3/src/3.4.cpp
@@ -10,19 +10,23 @@ int main() {
     // Task A
     cout << "Hva er ditt første ord?" << endl;
     cin >> word1;
+    cout << "[Fikk inn: " << word1 << " ]" << endl;
     cout << "Hva er ditt andre ord?" << endl;
     cin >> word2;
+    cout << "[Fikk inn: " << word2 << " ]" << endl;
     cout << "Hva er ditt tredje ord?" << endl;
     cin >> word3;
+    cout << "[Fikk inn: " << word3 << " ]" << endl;
     
     // Task  B
     string sentence = word1 + " " +  word2 + " " +  word3 + ".";
+    cout << "Vi har setningen: " << sentence << endl;
     
     // Task C
-    cout << "Word 1 lenght: " << word1.length() << endl;
-    cout << "Word 2 lenght: " << word2.length() << endl;
-    cout << "Word 3 lenght: " << word3.length() << endl;
-    cout << "Sentence lenght: " << sentence.length() << endl;
+    cout << "Word 1 har lengden: " << word1.length() << endl;
+    cout << "Word 2 har lengden: " << word2.length() << endl;
+    cout << "Word 3 har lengden: " << word3.length() << endl;
+    cout << "Setningen har lengden: " << sentence.length() << endl;
 
     // Task D
     string sentence2 = sentence;
@@ -46,6 +50,6 @@ int main() {
     for (int i = 0; i < sentence.size(); i++)
         if (sentence[i] == 'e' && sentence[i+1] == 'r') count++;
 
-    cout << "'er' appears " << count << " times in the sentence" << endl;
+    cout << "'er' dukker opp " << count << " ganger i setningen" << endl;
 
 }
