1f53b51d0a31e5982abcd77ad7daf55fe9768984
Christoffer Brevik <christoffer.brevik@tveco.net>
2023-09-21 13:12:29 +0200

    Autogenerated files

--- Task4/cmake-build-debug/cmake_install.cmake
+++ Task4/cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/password_is_12345678/Desktop/NTNU Oppgaver/5. Semester/C++/Task3
+# Install script for directory: /home/password_is_12345678/Desktop/NTNU Oppgaver/5. Semester/C++/Task4
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -50,5 +50,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/password_is_12345678/Desktop/NTNU Oppgaver/5. Semester/C++/Task3/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/password_is_12345678/Desktop/NTNU Oppgaver/5. Semester/C++/Task4/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
