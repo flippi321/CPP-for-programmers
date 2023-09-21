4bbebff71247b974838d87911896fa23c8e90877
Christoffer Brevik <christoffer.brevik@tveco.net>
2023-09-21 18:32:31 +0200

    Made it so that the button is disabled unless both fields are filled

--- Task4/src/4.2.cpp
+++ Task4/src/4.2.cpp
@@ -19,23 +19,22 @@ public:
 
         add(box);   // Add vbox to window
         show_all(); // Show all widgets
+        button.set_sensitive(false); // Disable button before the user enters anything
 
         // If the first name is changed, we check if inputs are valid
         firstName.signal_changed().connect([this]() {
             validInputs = (!firstName.get_text().empty() && !lastName.get_text().empty());
-            if(validInputs){
-                button.set_label("True");
-            }
+            button.set_sensitive(validInputs);
         });
 
         // If the last name is changed, we check if inputs are valid
         lastName.signal_changed().connect([this]() {
             validInputs = (!firstName.get_text().empty() && !lastName.get_text().empty());
-            if(validInputs){
-                button.set_label("True");
-            }
+            button.set_sensitive(validInputs);
         });
 
+        // If button is clicked we combine the first and last name
+        // Will only work if button is active (sensitive)
         button.signal_clicked().connect([this]() {
             Glib::ustring labelText = firstName.get_text() + " " + lastName.get_text();
             label.set_text(labelText);
