a4114cf9e10adbef984df8b10bcd4d287fa7b547
Christoffer Brevik <christoffer.brevik@tveco.net>
2023-10-02 10:16:58 +0200

    Copied files from task 8 to task 9

---
+++ Task 9/src/8.2.cpp
@@ -0,0 +1,26 @@
+#include "pair.hpp"
+#include <iostream>
+
+int main() {
+    Pair<double, int> p1(3.5, 14);
+    Pair<double, int> p2(2.1, 7);
+    cout << "p1: " << p1.first << ", " << p1.second << endl;
+    cout << "p2: " << p2.first << ", " << p2.second << endl;
+
+    if (p1 > p2)
+        cout << "p1 er størst" << endl;
+    else
+        cout << "p2 er størst" << endl;
+
+    auto sum = p1 + p2;
+    cout << "Sum: " << sum.first << ", " << sum.second << endl;
+
+    return 0;
+}
+
+/* Utskrift:
+p1: 3.5, 14
+p2: 2.1, 7
+p1 er størst
+Sum: 5.6, 21
+*/
