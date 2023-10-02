424ad68680686c9f9deb9fa43c899438bac55bf0
Christoffer Brevik <christoffer.brevik@tveco.net>
2023-10-02 09:04:15 +0200

    Copied files from Task 7

---
+++ Task 8/src/7.1.cpp
@@ -0,0 +1,24 @@
+//
+// fraction/main.cpp
+//
+#include "fraction.hpp"
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+void print(const string &text, const Fraction &broek) {
+  cout << text << broek.numerator << " / " << broek.denominator << endl;
+}
+
+int main() {
+  Fraction a(10, 20);
+  Fraction b(3, 4);
+  Fraction c = a - 5;
+  Fraction d = 5 - b;
+
+  print("a = ", a);
+  print("b = ", b);
+  print("c = ", c);
+  print("d = ", d);
+}
