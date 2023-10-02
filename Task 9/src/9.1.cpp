270e7a3309f04f794780727653ca0d82a1f25133
Christoffer Brevik <christoffer.brevik@tveco.net>
2023-10-02 10:44:50 +0200

    Added task 9a and 9b

--- Task 9/src/9.1.cpp
+++ Task 9/src/9.1.cpp
@@ -1,48 +1,49 @@
-//
-// fraction/main.cpp
-//
+#include <algorithm>
 #include <iostream>
-#include <cmath>
-#include <iomanip>
+#include <string>
+#include <vector>
 
 using namespace std;
 
-// Task specified the '==' operator would be defined by the values
-template <typename Type> bool equal(Type a, Type b) {
-    return (a==b);
+ostream &operator<<(ostream &out, const vector<int> &table) {
+    for (auto &e : table)
+        out << e << " ";
+    return out;
 }
 
-// Specialized method for double values
-bool equal(double a, double b) {
-    const double toleranse = 0.00001;
-    if (fabs(a - b) < toleranse) {
-        return true;
-    }
-    return false;
+// Function used in 9b for checking if 2 values are "approximately equal"
+bool isApproximatelyEqual(int a, int b) {
+    return abs(a - b) <= 2;
 }
 
-// Template method using any of the two templates and prints out the response
-template <typename Type> void printEqual(Type a, Type b) {
-    if (equal(a, b)){
-        cout << a << " equals " << setprecision(7) << b << endl;
+void printEquality(bool isEqual, int number){
+    if (isEqual) {
+        cout << "Intervallet [v1.begin(), v1.begin() + " << number <<"> og v2 er omtrent like." << endl;
     } else {
-        cout << a << " does not equal " << b << endl;
+        cout << "Intervallet [v1.begin(), v1.begin() + 5> og v2 er ikke omtrent like." << endl;
     }
 }
 
 int main() {
-  double a = 1.5;
-  double b1 = 1.50001;
-  double b2 = 1.500001;
+    vector<int> v1 = {3, 3, 12, 14, 17, 25, 30};
+    cout << "v1: " << v1 << endl;
+    vector<int> v2 = {2, 3, 12, 14, 24};
+    cout << "v2: " << v2 << endl;
 
-  int c = 1;
-  int d1 = 1;
-  int d2 = 2;
+    // A:
+    auto it = find_if(v1.begin(), v1.end(), [](int element) {
+        return element > 15;
+    });
 
-  printEqual(a, b1);
-  printEqual(a, b2);
-  printEqual(c, d1);
-  printEqual(c, d2);
+    if (it != v1.end()) {
+        cout << "Det første elementet i v1 som er større enn 15: " << *it << endl;
+    } else {
+        cout << "Ingen elementer i v1 er større enn 15." << endl;
+    }
 
-  return 0;
+    // B:
+    bool isApproximatelyEqualInterval1 = equal(v1.begin(), v1.begin() + 5, v2.begin(), isApproximatelyEqual);
+    bool isApproximatelyEqualInterval2 = equal(v1.begin(), v1.begin() + 4, v2.begin(), isApproximatelyEqual);
+    printEquality(isApproximatelyEqualInterval1, 5);
+    printEquality(isApproximatelyEqualInterval2, 4);
 }
